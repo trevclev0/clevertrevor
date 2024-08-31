551bdd1771b1379db38f74de0b05cdf96578d78b
Trevor Duurán <trevor.m.duran@gmail.com>
2024-08-30 23:08:06 -0600

    feat: add text next to logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as Logo } from '../../assets/logo.svg'
 function Navbar() {
   return <>
     <nav>
-      <Link to="/"><Logo height={28} width={28} /></Link>
+      <Link to="/"><Logo height={28} width={28} />Trevor Dur&aacute;n</Link>
       <ul>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/resume">R&eacute;sum&eacute;</Link></li>
