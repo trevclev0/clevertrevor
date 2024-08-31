99a935af140e8224568b9f35e85db3c2a3d3d874
Trevor Durán <trevor.m.duran@gmail.com>
2024-08-31 12:09:35 -0600

    chore: configure eslint correctly

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,30 @@
-import './Navbar.css';
-import { Link } from 'react-router-dom'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 function Navbar() {
-  return <>
+  return (
     <nav>
-      <Link className='logo-link' to="/"><Logo height={28} width={28} />Trevor Dur&aacute;n</Link>
+      <Link className="logo-link" to="/">
+        <Logo height={28} width={28} />
+        Trevor Dur&aacute;n
+      </Link>
       <ul>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/resume">R&eacute;sum&eacute;</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/resume">R&eacute;sum&eacute;</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
-    </nav >
-  </>
+    </nav>
+  );
 }
 
 export default Navbar;
