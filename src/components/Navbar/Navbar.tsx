19d677ddf01ce673bf432b22812bc4577979149f
Trevor Durán <trevor.m.duran@gmail.com>
2024-09-05 15:10:53 -0600

    feat: use custom link component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,29 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Navlink from "../Navlink/Navlink";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 function Navbar() {
   return (
-    <nav>
-      <Link className="logo-link" to="/">
+    <nav className="flex justify-between items-center pv2 fw5 overflow-hidden">
+      <Link className="dn flex items-center fw8 link" to="/">
         <Logo height={28} width={28} />
-        <p>Trevor Dur&aacute;n</p>
+        <p className="ml2 muted-blue">Trevor Dur&aacute;n</p>
       </Link>
-      <ul>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/resume">R&eacute;sum&eacute;</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+      <ul className="flex justify-around">
+        <Navlink href="/projects">Projects</Navlink>
+        <Navlink href="/resume">R&eacute;sum&eacute;</Navlink>
+        <Navlink href="/about">About</Navlink>
+        <Navlink href="/contact">Contact</Navlink>
+        <button
+          type="button"
+          className="flex icon input-reset pointer muted-blue f4"
+          aria-label="Menu"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
       </ul>
     </nav>
   );
